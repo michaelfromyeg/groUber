18e599baa4efa82dd9c5b4da3b4ba96cff75a7e7
Jack He <jackhe2345@gmail.com>
2020-08-01 13:09:23 -0700

    done plotting map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,8 +1,19 @@
 import React, { ReactElement } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import theme from './theme';
+import useEventPeople from '../../hooks/useEventPeople'
+import { useParams } from 'react-router-dom';
+import { useDocument } from 'react-firebase-hooks/firestore';
+import firebase from 'firebase';
 
 const MapContainer = ({ google, center }: { google: any; center: google.maps.LatLngLiteral }): ReactElement => {
+    const { eventId } = useParams();
+    const [eventData, loading, error] = useDocument(firebase.firestore().collection('events').doc(eventId), {
+        snapshotListenOptions: {
+            includeMetadataChanges: true,
+        },
+    });
+    const members = useEventPeople(eventData)
     return (
         <Map
             google={google}
@@ -10,7 +21,17 @@ const MapContainer = ({ google, center }: { google: any; center: google.maps.Lat
             styles={theme}
             initialCenter={center}
             containerStyle={{ height: 'calc(100vh - 64px)', width: '82.5vw' }}
-        />
+        >
+        {members.map((person) => {
+            return (
+                <Marker
+                    label = {person.name}
+                    position = {person.location.latlng}
+                >
+                </Marker>
+            )
+        })}
+        </Map>
     );
 };
 
