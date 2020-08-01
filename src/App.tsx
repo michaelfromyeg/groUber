c9ea921242fd645877e5bc17903faa07e889674d
Michael DeMarco <michaelfromyeg@gmail.com>
2020-08-01 10:51:46 -0600

    Fixing prettier and running linting through codebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import React, { useState, ReactElement } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Index from './pages/index';
 import Dashboard from './pages/dashboard';
 import Form from './pages/form';
@@ -17,59 +12,56 @@ import * as firebase from 'firebase';
 import 'firebase/auth';
 
 function PrivateRoute({ component: Component, ...rest }: any) {
-  return (
-    <Route
-      {...rest}
-      render={(props) => {
-        if (firebase.auth().currentUser) {
-          return <Component {...props} />;
-        } else {
-          window.localStorage.setItem('location', props?.location?.pathname);
-          return <Redirect to="/login" />;
-        }
-      }}
-    />
-  );
+    return (
+        <Route
+            {...rest}
+            render={(props) => {
+                if (firebase.auth().currentUser) {
+                    return <Component {...props} />;
+                } else {
+                    window.localStorage.setItem('location', props?.location?.pathname);
+                    return <Redirect to="/login" />;
+                }
+            }}
+        />
+    );
 }
 
 function PublicRoute({ component: Component, onUserLogin, ...rest }: any) {
-  return (
-    <Route
-      {...rest}
-      render={(props) => {
-        if (!firebase.auth().currentUser) {
-          return <Component {...props} onUserLogin={onUserLogin} />;
-        } else {
-          const route = window.localStorage.getItem('location') || '/';
-          window.localStorage.removeItem('location');
-          return <Redirect to={route} />;
-        }
-      }}
-    />
-  );
+    return (
+        <Route
+            {...rest}
+            render={(props) => {
+                if (!firebase.auth().currentUser) {
+                    return <Component {...props} onUserLogin={onUserLogin} />;
+                } else {
+                    const route = window.localStorage.getItem('location') || '/';
+                    window.localStorage.removeItem('location');
+                    return <Redirect to={route} />;
+                }
+            }}
+        />
+    );
 }
 
 const App = (): ReactElement => {
-  const [user, setUser] = useState(firebase.auth().currentUser);
-  firebase.auth().onAuthStateChanged(setUser);
+    const [user, setUser] = useState(firebase.auth().currentUser);
+    firebase.auth().onAuthStateChanged(setUser);
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Switch>
-          <PublicRoute exact path="/login" component={Login} />
-          <PrivateRoute exact path="/" component={Index} />
-          <PrivateRoute
-            path="/event/:eventId/dashboard"
-            component={Dashboard}
-          />
-          <PrivateRoute path="/event/:eventId/form" component={Form} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-        <Notification />
-      </Router>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Router>
+                <Switch>
+                    <PublicRoute exact path="/login" component={Login} />
+                    <PrivateRoute exact path="/" component={Index} />
+                    <PrivateRoute path="/event/:eventId/dashboard" component={Dashboard} />
+                    <PrivateRoute path="/event/:eventId/form" component={Form} />
+                    <Route path="*" component={NotFound} />
+                </Switch>
+                <Notification />
+            </Router>
+        </ThemeProvider>
+    );
 };
 
 export default App;
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,25 +2,19 @@ import React, { ReactElement } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import theme from './theme';
 
-const MapContainer = ({
-  google,
-  center
-}: {
-  google: any;
-  center: google.maps.LatLngLiteral;
-}): ReactElement => {
-  return (
-    <Map
-      google={google}
-      zoom={13}
-      styles={theme}
-      initialCenter={center}
-      containerStyle={{ height: 'calc(100vh - 64px)', width: '82.5vw' }}
-    />
-  );
+const MapContainer = ({ google, center }: { google: any; center: google.maps.LatLngLiteral }): ReactElement => {
+    return (
+        <Map
+            google={google}
+            zoom={13}
+            styles={theme}
+            initialCenter={center}
+            containerStyle={{ height: 'calc(100vh - 64px)', width: '82.5vw' }}
+        />
+    );
 };
 
 export default GoogleApiWrapper((props) => ({
-  ...props,
-  apiKey: 'AIzaSyD2mfMm4KHPGErUYWr3bNFwALDVP-EKkXc'
+    ...props,
+    apiKey: 'AIzaSyD2mfMm4KHPGErUYWr3bNFwALDVP-EKkXc',
 }))(MapContainer);
