5ac0f0241e42c16e6a524c4dc43e4aaa8fad1929
Hasan Altaf <hasanaltaf2001@gmail.com>
2020-08-01 11:11:06 -0700

    add link view component

---
+++ src/components/LinkSharing.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }),
+);
+
+
+export default function FormPropsTextFields(props: any) {
+    const classes = useStyles();
+    return (
+        <form className={classes.root} noValidate autoComplete="off">
+            <TextField
+                    id="standard-read-only-input"
+                    label="Read Only"
+                    defaultValue={props.link}
+                    InputProps={{
+                        readOnly: true,
+                    }}
+                    />
+        </form>
+    )
+}
