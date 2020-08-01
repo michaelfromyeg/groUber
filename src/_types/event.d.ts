dff1161465b7b13c20574d2260d1dcdf2562b15c
Phillbert Tan <philly@phillytan.xyz>
2020-08-01 13:03:54 -0700

    Refactor how we store users + event date

--- src/_types/event.d.ts
+++ src/_types/event.d.ts
@@ -2,14 +2,17 @@ import { People } from './people';
 
 export interface Event {
     _id?: string;
-    name: string;
-    organizerEmail: string;
-    people: People[];
-    destination: {
-        address: string;
-        latlng: {
-            lat: double;
-            lng: double;
+    name?: string;
+    organizerEmail?: string; // TODO: deprecate
+    organizerName?: string; // TODO: deprecate
+    date?: Date;
+    host?: People;
+    people?: People[];
+    destination?: {
+        address?: string;
+        latlng?: {
+            lat?: double;
+            lng?: double;
         };
     };
     path?: any; // TODO:
