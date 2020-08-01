c8f0bb931372384a6a5869ad686d2bc3c6a8f238
Phillbert Tan <philly@phillytan.xyz>
2020-08-01 01:31:32 -0700

    Refactor Google Maps Widget

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,35 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import * as firebase from 'firebase';
-import { Event } from '../_types/event'
+import { Event } from '../_types/event';
 import { useParams, useHistory } from 'react-router-dom';
 import { useDocument } from 'react-firebase-hooks/firestore';
-import { AppBar, Toolbar, IconButton, Typography, Button, makeStyles } from '@material-ui/core'
-import classes from '*.module.css'
-import BackIcon from '@material-ui/icons/ArrowBackIosRounded'
-import Map from '../components/Map'
-import ListView from '../components/ListView'
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  makeStyles
+} from '@material-ui/core';
+import classes from '*.module.css';
+import BackIcon from '@material-ui/icons/ArrowBackIosRounded';
+import Map from '../components/Map';
+import ListView from '../components/ListView';
 
 const useStyles = makeStyles((theme) => ({
   menuButton: {
     marginRight: theme.spacing(2)
   }
-}))
+}));
 
 //eslint-disable-next-line
 const globalAny: any = global
 // some shit
 const DashBoard = () => {
-
-  const classes = useStyles()
+  const classes = useStyles();
   const { eventId } = useParams();
   const history = useHistory();
   const [coord, setCoord] = useState({
-    lat: 0.0, 
+    lat: 0.0,
     lng: 0.0
-  })
+  });
 
   const [value, loading, error] = useDocument(
-    firebase.firestore().collection("events").doc(eventId),
+    firebase.firestore().collection('events').doc(eventId),
     {
       snapshotListenOptions: {
         includeMetadataChanges: true
@@ -38,32 +44,30 @@ const DashBoard = () => {
   );
 
   if (!loading && !value?.data()) {
-    globalAny.setNotification('error', 'Event not found.')
-    history.push('/')
+    globalAny.setNotification('error', 'Event not found.');
+    history.push('/');
   }
 
-  const event = value?.data()
-  
+  const event = value?.data();
 
   React.useEffect(() => {
     const showPosition = (position: any) => {
-      const lat = position.coords.latitude
-      const lng = position.coords.longitude
-      console.log(lat, lng)
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+      console.log(lat, lng);
       setCoord({
         lat: lat,
         lng: lng
-      })
-    }
-  
+      });
+    };
+
     // get user current location
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-      alert("your browser doesn't support maps") 
+      alert("your browser doesn't support maps");
     }
-  }, []) 
-  
+  }, []);
 
   return (
     <>
@@ -83,17 +87,14 @@ const DashBoard = () => {
           <Typography variant="h6">groUber</Typography>
         </Toolbar>
       </AppBar>
-      <div style = {{display: "flex"}}>
+      <div style={{ display: 'flex' }}>
         {/* Load map */}
-        <Map
-          center={coord}
-          zoom={13}
-        />
+        <Map center={coord} />
         {/* Load side-menu */}
         <ListView />
       </div>
     </>
   );
-}
+};
 
 export default DashBoard;
