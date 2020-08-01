c1c5e0a75715857172c8020f7c99510d9fa88791
liang303 <stuffbyliang@gmail.com>
2020-08-01 15:22:31 -0700

    finished algorithm but didnt test it
    Co-authored-by: Hasan Altaf <hasanaltaf2001@gmail.com>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,19 @@
 import * as functions from 'firebase-functions';
 import { Event } from '../../src/_types/event'
 import { People } from '../../src/_types/people'
+import haversine from 'haversine';
 
-const { priorityQueue } = require('./priorityQueue')
+// import { PriorityQueue, Node } from './PriorityQueue'
+import PriorityQueue from 'priorityqueue';
 
 
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request: any, response: any) => {
+export const solve = functions.https.onRequest((request: any, response: any) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  response.json(solveCarpoolProblem(request.body.event));
 });
 
 /** 
@@ -23,8 +25,139 @@ export const helloWorld = functions.https.onRequest((request: any, response: any
  * @returns Map<string, Array<string>>
  */
 function solveCarpoolProblem(event: Event): Map<string, Array<string>> {
-  // TODO:
-  return new Map();
+  const people = event.people;
+  let remainingDrivers = getDrivers(people);
+  let remainingPassengers = getPassengers(people);
+
+  const solution = new Map<string, Array<string>>();
+
+  while (!isDone(remainingDrivers, remainingPassengers)) {
+    // const distances = calculatePassengerDistances(remainingDrivers, remainingPassengers);
+
+    const closestDriverToPassengers = new Map<string, [string, number]>();
+
+    for(let passenger of remainingPassengers) {
+      let minDriverDistance = Infinity;
+      let minDriverId = "";
+      for(let driver of remainingDrivers) {
+        const distance = haversine({
+          latitude: driver.location.latlng.lat,
+          longitude: driver.location.latlng.lng,
+        }, {
+          latitude: passenger.location.latlng.lat,
+          longitude: passenger.location.latlng.lng,
+        }, {unit: 'meter'})
+
+        if(distance < minDriverDistance) {
+          minDriverDistance = distance;
+          minDriverId = driver.id;
+        }
+      }
+
+      closestDriverToPassengers.set(passenger.id, [minDriverId, minDriverDistance]);
+    }
+
+
+    let pq = new PriorityQueue();
+
+
+    // this may be fucked
+    const numericCompare = (a:number, b:number) => (a > b ? 1 : a < b ? -1 : 0);
+
+    const comparator = (a: any, b: any) => {
+      return numericCompare(a.minDriverDistance, b.minDriverDistance);
+    };
+
+    for(let passenger of remainingPassengers) {
+      const array = closestDriverToPassengers.get(passenger.id);
+      if (!array) continue;
+      const [minDriverId, minDriverDistance] = array;
+      
+      let node = {
+        passengerId: passenger.id,
+        minDriverId,
+        minDriverDistance,
+      }
+
+      pq.enqueue(node);
+    }
+
+    const nextPassengerNode: any = pq.dequeue();
+    if(!nextPassengerNode) continue;
+    const nextPassenger = getPersonById(people, nextPassengerNode.value.passengerId)
+    const minDriver = getPersonById(people, nextPassengerNode.value.minDriverId);
+
+    if(!nextPassenger || !minDriver) continue;
+
+    minDriver.location.latlng = {
+      lat: nextPassenger?.location.latlng.lat,
+      lng: nextPassenger?.location.latlng.lng
+    }
+
+    const passengerIds: string[] = solution.get(minDriver.id) || [];
+    passengerIds.push(nextPassenger.id)
+    solution.set(minDriver.id, passengerIds);
+
+    minDriver.seats--;
+    if(minDriver.seats === 0) {
+      remainingDrivers = removePerson(remainingDrivers, minDriver.id);
+    }
+    remainingPassengers = removePerson(remainingPassengers, nextPassenger.id);
+    // TODO add to database for visualization of this algorithm
+
+    // no more remaining passengers but still have drivers
+    if(remainingPassengers.length===0) {
+      let driverToBeConverted = getSomeoneDrivingNoOne(remainingDrivers, solution);
+      if (driverToBeConverted) {
+        // exists
+        remainingPassengers.push(driverToBeConverted);
+        removePerson(remainingDrivers, driverToBeConverted.id)
+      } else {
+        break;
+      }
+    }
+  }
+
+  return solution;
+}
+
+function getSomeoneDrivingNoOne(drivers: People[], solution: Map<string, Array<string>>): People | undefined {
+  for(let driver of drivers) {
+    if(solution.has(driver.id)) {
+      return driver;
+    }
+  }
+  return undefined;
+}
+
+function removePerson(people : People[], id: string) : People[] {
+  for(let i=0; i<people.length; i++) {
+    let driver = people[i];
+    if (driver.id === id) {
+      people.splice(i, 1);
+    }
+  }
+  return people;
+}
+
+function getPersonById(people:People[], id:string): People | undefined {
+  for(const person of people) {
+    if (person.id === id) {
+      return person;
+    }
+  }
+  return undefined;
+}
+
+/**
+ * returns whether the solution contains all the people in an event
+ * 
+ * @param  {People} people
+ * @param  { Map<string, Array<string>>} solution
+ * @returns boolean
+ */
+function isDone(drivers: People[], passengers: People[]) : boolean {
+  return drivers.length === 0 || passengers.length === 0;
 }
 
 /** 
@@ -34,7 +167,9 @@ function solveCarpoolProblem(event: Event): Map<string, Array<string>> {
  * @returns People[]
  */
 function getDrivers(people: People[]): People[] {
-  return [];
+  return people.filter((person) => {
+    return person.canDrive;
+  })
 }
 
 /** 
@@ -44,47 +179,8 @@ function getDrivers(people: People[]): People[] {
  * @returns People[]
  */
 function getPassengers(people: People[]): People[] {
-  return [];
+  return people.filter((person) => {
+    return !person.canDrive;
+  })
 }
-
-/**
- * Return a dict where the key is a driver id, and the value associated
- * is a dict with passenger id and distance to that passenger
- * 
- * @param  {People[]} drivers
- * @param  {People[]} passengers
- * @returns Map<string, Map<string, number>>
- */
-function calculateDriverDistances(drivers: People[], passengers: People[])
-  : Map<string, Map<string, number>> {
-  return null;
-}
-
-/**
- * Return a dict where the key is a passenger id, and the value associated
- * is a dict with driver id and distance to that driver
- * 
- * @param  {People[]} drivers
- * @param  {People[]} passengers
- * @returns Map<string, Map<string, number>>
- */
-function calculatePassengerDistances(drivers: People[], passengers: People[])
-  : Map<string, Map<string, number>> {
-  return null;
-}
-// function findNextPassenger(): People
-
-function updateDriversAndPassengers(drivers: People[], passengers: People[], driver, passenger)
-
-// function 
-
-
-/** Returns the person that is the farthest away from the middle point of all availalbe drivers
- * @param  {People[]} people
- * @returns People
- */
-function findFarthestFromMiddle(people: People[]): People {
-  return null;
-}
-
 
