9959e5c1c9cd0dedbd6fe3feb81cf8ac00ebca40
Michael DeMarco <michaelfromyeg@gmail.com>
2020-08-02 02:46:49 -0600

    Updating the README.md, adding an SVG logo, matching content with Devpost, adding 'whitepaper'

--- README.md
+++ README.md
@@ -1,35 +1,82 @@
 <p align="center">
-  <img src="branding/logo/logo192.png">
+  <img width="15%" src="branding/logo/grouber-logo-01.svg">
 </p>
 
-# groUber
+# groUber: schedule carpools, without the headache
 
-Get your gang together, without the headache. [To the Moon and Hack Submission]
+Hello, world! We’re groUber, an app for organizing events in the 21st century. 
 
-groups + Uber = groUber
+groups + Uber = [groUber](https://grouber.online)
 
 ## About
 
-This project is aimed to help event planners create carpools, and is being built for To the Moon and Hack. If you're going to use this project to plan your event, remember to stay 6ft apart!
+groUber is aimed to help event planners create carpools, and is being built for To the Moon and Hack. If you're going to use this project to plan your event, remember to stay 6ft apart!
+
+### Motivation
+
+This project was built by a group of 5 students from UBC in Vancouver, BC who love automating things. And one horrific task all of us have run into when planning our events is creating a workable carpool schedule.
+
+It’s a great option for getting your group together: whether it be parents figuring out how best to get their kids to soccer practice, or friends accommodating those without access to a car, carpooling is common, but creating a plan can be painful, to say the least.
+
+You finally come up with a workable schedule: everyone can make it to the event, no driver has to go in annoying, wasteful loops, and everything can start on time.
+
+Then a driver with 4 seats drops out. And you have to do it all over again. No, thanks.
+
+### Introducing: groUber
+
+With groUber, never go through that headache again. As an event organizer, create your event, send an invite link to your friends, and create a carpool schedule with one click. As a participant, simply receive a link, RSVP, and inform the organizer of how many seats you have available. You’ll receive a schedule on the day-of.
+
+Using the Radar API and the Google Maps API, along with a bit of algo-magic, our app will create the most optimal carpool schedule for everyone involved. We were hesitant to do this project at first; the idea of designing an algorithm to find the “best” carpool strategy was intimidating to say the least. After some research, it turns out this is actually classified an NP-hard problem. We didn't need to solve the problem generally though, and were able to design a heuristic algorithm to be able to compute this with fairly good results. Here are a [few](https://www.sciencedirect.com/science/article/pii/S1877050914006334) [examples](https://arxiv.org/pdf/1604.05609.pdf) of scholarly work in this area. It took great teamwork, persistence, and a decent amount of caffeine to get this working.
+
+Now, drivers won’t have to waste gas, and everyone will get there on time. Someone drops out? No problem, our app will allow you to adjust your schedule, painlessly. 
+
+groUber is ride-sharing for your group of friends, without all the expenses and overhead. Do a favor for the environment, and for your stress-levels, and start using groUber today.
+
+Here's our [whitepaper](https://docs.google.com/document/d/e/2PACX-1vSLo8DhZ7p-VgPnmieasD01zZ2_76uMxPxSwVErq3_gQPvpokrFI4G5SHA1Pxrc9b9ouhK5yvL-4X3t/pub) where you can find a more in-depth justification for this hack!
 
 ## Installation
 
 ### Stack
 
 - TypeScript, React
-- NPM
-- Firebase
+- Node.js, npm
+- Firebase, including authentication, hosting, and Firestore
 
-### Get in running
+### Get it running
 
-Run `npm start` for development.
+`npm install`
+Install dependencies.
 
-Deployment happens via GitHub Actions.
+`npm start`
+Run for development.
+
+`npm lint`
+Run linting over the project repository.
+
+Deployment happens entirely via GitHub Actions; on any pushes to master, the app will be re-deployed to Firebase hosting.
 
 ## Usage
 
-Add your participants data (address, can they drive, and number of seats in their car), and let the app go to work.
+Head to [grouber.online](https://grouber.online). Sign-up using your Google account.
+
+Create your event with all key details, and send an invite to your to-be attendees. Once they RSVP, you'll see their details on your event dashboard. Then, generate your event's carpool schedule in one click!
+
+Participants must submit their address, if they're driving, and if so, how many seats they have available in their vehicle.
+
+### Examples
+
+<p align="center">
+  <img width="50%" src="images/branding.png">
+</p>
+
+<p align="center">
+  <img width="50%" src="images/homescreen.png">
+</p>
+
+<p align="center">
+  <img width="50%" src="images/dashboard.png">
+</p>
 
 ## Contributing
 
-This will be updated after the hack-a-thon! Stay posted for more.
+This will be updated after the hackathon! Stay posted for more.
--- functions/src/DistanceMatrix.ts
+++ functions/src/DistanceMatrix.ts
@@ -1,99 +1,98 @@
-import { People } from "../../src/_types/people";
-import axios from "axios";
+import { People } from '../../src/_types/people';
+import axios from 'axios';
 
 export interface Location {
-  latitude: number,
-  longitude: number
+    latitude: number;
+    longitude: number;
 }
 
 export class DistanceMatrix {
-  people: People[]
-  data: CustomMap<CustomMap<number>> = new CustomMap() // number is the distance
-  /**
-   * @param  {People[]} people
-   * @example distanceMatrix = new DistanceMatrix(people)
-   */
-  constructor(people: People[]) {
-    this.people = people;
-  }
-  /** generates data which is our map given a list of people.
-   * 
-   * @example await distanceMatrix.init();
-   */
-  async init() {
-    const apiData = await this.grabMatrixDataFromRadar(this.people);
-    const origins = apiData.origins;
-    const destinations = apiData.destinations;
-    const matrix = apiData.matrix;
-   
-    for (let i = 0; i < origins.length; i++){
-      let distances: CustomMap<number> = new CustomMap()
+    people: People[];
+    data: CustomMap<CustomMap<number>> = new CustomMap(); // number is the distance
+    /**
+     * @param  {People[]} people
+     * @example distanceMatrix = new DistanceMatrix(people)
+     */
+    constructor(people: People[]) {
+        this.people = people;
+    }
+    /** generates data which is our map given a list of people.
+     *
+     * @example await distanceMatrix.init();
+     */
+    async init() {
+        const apiData = await this.grabMatrixDataFromRadar(this.people);
+        const origins = apiData.origins;
+        const destinations = apiData.destinations;
+        const matrix = apiData.matrix;
 
-      let startLoc: Location = {
-        latitude: this.people[i].location.latlng.lat,
-        longitude: this.people[i].location.latlng.lng,
-      }
-      for (let j = 0; j < destinations.length; j++){
-        let endLoc: Location = {
-          latitude: this.people[j].location.latlng.lat,
-          longitude: this.people[j].location.latlng.lng,
+        for (let i = 0; i < origins.length; i++) {
+            const distances: CustomMap<number> = new CustomMap();
+
+            const startLoc: Location = {
+                latitude: this.people[i].location.latlng.lat,
+                longitude: this.people[i].location.latlng.lng,
+            };
+            for (let j = 0; j < destinations.length; j++) {
+                const endLoc: Location = {
+                    latitude: this.people[j].location.latlng.lat,
+                    longitude: this.people[j].location.latlng.lng,
+                };
+                distances.set(endLoc, matrix[i][j].distance.value);
+            }
+            this.data.set(startLoc, distances);
         }
-        distances.set(endLoc, matrix[i][j].distance.value);      
-      }
-      this.data.set(startLoc, distances)
-    } 
-  }
+    }
 
-  async grabMatrixDataFromRadar(people: People[]): Promise<any> {
-    let locations: string[] = people.map((person) => {
-      return `${person.location.latlng.lat},${person.location.latlng.lng}`;
-    })
+    async grabMatrixDataFromRadar(people: People[]): Promise<any> {
+        const locations: string[] = people.map((person) => {
+            return `${person.location.latlng.lat},${person.location.latlng.lng}`;
+        });
 
-    let result = await axios.get('https://api.radar.io/v1/route/matrix', {
-      params: {
-        origins: locations.join('|'),
-        destinations: locations.join('|'),
-        mode: "car",
-        units: "metric"
-      },
-      headers: {
-        'Authorization': `prj_live_pk_7a9bbe078da0cfa051f77e2c9d9d0f929b9e5955`
-      }
-    })
+        const result = await axios.get('https://api.radar.io/v1/route/matrix', {
+            params: {
+                origins: locations.join('|'),
+                destinations: locations.join('|'),
+                mode: 'car',
+                units: 'metric',
+            },
+            headers: {
+                Authorization: `prj_live_pk_7a9bbe078da0cfa051f77e2c9d9d0f929b9e5955`,
+            },
+        });
 
-    return result.data;
-  }
+        return result.data;
+    }
 }
 
 export class CustomMap<B> {
-  // fields go here
-  // id: string
+    // fields go here
+    // id: string
 
-  keys: Array<Location> = []
-  values: Array<B> = [];
+    keys: Array<Location> = [];
+    values: Array<B> = [];
 
-  set(key: Location, value:B) {
-    var index = this.findIndex(key);
-    if(index == -1) {
-      this.keys.push(key);
-      this.values.push(value);
+    set(key: Location, value: B) {
+        const index = this.findIndex(key);
+        if (index == -1) {
+            this.keys.push(key);
+            this.values.push(value);
+        } else {
+            this.values[index] = value;
+        }
     }
-    else {
-      this.values[index] = value;
-    }
-  }
 
-  get(key: Location) {
-    return this.values[this.findIndex(key)];
-  }
-
-  findIndex(key: Location): number {
-    for(let i=0; i<this.keys.length; i++) {
-      let item = this.keys[i];
-      if (item.latitude === key.latitude && item.longitude === key.longitude) {
-        return i;
-      }
+    get(key: Location) {
+        return this.values[this.findIndex(key)];
     }
-    return -1;
-  }
+
+    findIndex(key: Location): number {
+        for (let i = 0; i < this.keys.length; i++) {
+            const item = this.keys[i];
+            if (item.latitude === key.latitude && item.longitude === key.longitude) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
